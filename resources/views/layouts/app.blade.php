3f90660ed9e68d25edc7d54ae9746ca3c5fcc28b
AldoJair2020 <aldo.jairmorales@outlook.com>
2023-10-10 14:11:09 -0600

    Se modificó el login y register

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>@yield('title') - GestionaTiUsers</title>
-    <link rel="icon" href="{{ asset('storage/imagenes/logo.ico') }}">
+    <link rel="icon" href="{{ asset('storage/imagenes/logo.png') }}">
 
     
      <!-- Tailwind CSS Link -->
